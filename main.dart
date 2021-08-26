e9556180a3ab139ad2cbe6c67d56d5a031d78636
Gabriel Amorim <57738235+gabriel-amorim1@users.noreply.github.com>
2021-08-26 19:58:54 -0300

    First commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Text(
+      'Olá, Mundo!',
+       textDirection: TextDirection.ltr,
+       style: TextStyle(
+        color: Colors.white,
+       ),
+    ), //Text.
+  ); //runApp.
+}
