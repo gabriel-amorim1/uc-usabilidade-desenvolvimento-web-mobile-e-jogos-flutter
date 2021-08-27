fa1d2ef363a927cc5f6f9fceb9b96a7af95d6823
Gabriel Amorim <57738235+gabriel-amorim1@users.noreply.github.com>
2021-08-26 21:14:16 -0300

    feat: use Center widget

--- lib/Pratica02/main.dart
+++ lib/Pratica02/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Text(
-      'Gabriel de Amorim',
-       textDirection: TextDirection.ltr,
-       style: TextStyle(
-        color: Colors.white,
-       ),
-    ), //Text.
+    Center(
+      child: Text(
+        'Gabriel de Amorim',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ), //Text.
+    ), //Center.
   ); //runApp.
-}
+}  
